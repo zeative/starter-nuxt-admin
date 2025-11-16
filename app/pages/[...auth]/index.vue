3d42f0378bce607fd9cd5c61c0edb21606dc7cf5
zeative <zaadevofc@gmail.com>
2025-11-16 18:53:37 +0700

    feat: implement authentication components for login, forgot, and reset functionalities with form validation

--- app/pages/[...auth]/index.vue
+++ app/pages/[...auth]/index.vue
@@ -1,3 +1,10 @@
+<script setup>
+const route = useRoute();
+const auth = route.params.auth.join('/');
+</script>
+
 <template>
-   <AuthLogin v-if="$route.params.auth?.join('/') == 'auth/login'" />
+  <AuthLogin v-if="auth == 'auth/login'" />
+  <AuthForgot v-if="auth == 'auth/forgot'" />
+  <AuthReset v-if="auth == 'auth/reset'" />
 </template>
