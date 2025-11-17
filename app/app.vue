0368e10cff6154d9d8b6e60775ea267b0b289401
zeative <zaadevofc@gmail.com>
2025-11-17 08:48:02 +0700

    fix: auth infinite redirect

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-await callOnce(async () => {
-  console.log('This will only be logged once');
-});
-</script>
-
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator :throttle="0" />
